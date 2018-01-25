678b54d1d6c4696c4608830ad928cbfd1ea00513
NabaShauk <nshaukat@oakland.edu>
2018-01-25 18:25:34 -0500

    Edit to login page
    Made some style edits to the login page. Rounded edges

--- pages/Login/login.php
+++ pages/Login/login.php
@@ -27,17 +27,26 @@
     <style>
     
         *{margin: 0; padding: 0;}
-        body{background: #ecf1f4; font-family: sans-serif;}
-        
-        .form-wrap{ width: 320px; background: #3e3d3d; padding: 40px 20px; box-sizing: border-box; position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%);}
-        h1{text-align: center; color: #fff; font-weight: normal; margin-bottom: 20px;}
+        body{background: #c1c1c1; font-family: sans-serif;}
+
         
         input{width: 100%; background: none; border: 1px solid #fff; border-radius: 3px; padding: 6px 15px; box-sizing: border-box; margin-bottom: 20px; font-size: 16px; color: #fff;}
         
-        input[type="button"]{ background: #bac675; border: 0; cursor: pointer; color: #3e3d3d;}
-        input[type="button"]:hover{ background: #a4b15c; transition: .6s;}
+        input[type="button"]{ background: #b3fce5; border: 0; cursor: pointer; color: #3e3d3d;}
+        input[type="button"]:hover{ background: #b3fce5; transition: .6s;}
         
-        ::placeholder{color: #fff;}
+        ::placeholder{color: #b3fce5;}
+
+
+        .rcorners2 {
+            border-radius: 25px;
+            border: 2px solid #73AD21;
+            padding: 40px;
+            width: 200px;
+            height: 250px;
+            position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%);
+        }
+
     
     </style>
 
@@ -52,6 +61,7 @@
             ?>
         </div>
         <div class="form-wrap">
+            <div class="rcorners2"
             <form action="login.php" method="POST">
                 <h1>Login</h1>
                 <input type="text" placeholder="E-Mail" name="email">
@@ -61,5 +71,6 @@
                 <input type="button" value="Forgot Password?">
             </form>
         </div>
+    </div>
     </body>
 </html>
