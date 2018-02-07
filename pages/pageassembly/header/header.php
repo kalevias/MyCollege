07ffac48c66654fa16fec130c1de9daede3082ba
NabaShauk <nshaukat@oakland.edu>
2018-02-07 13:34:28 -0500

    Fix Modal
    Fixed the Modal design

--- pages/pageassembly/header/header.php
+++ pages/pageassembly/header/header.php
@@ -83,22 +83,67 @@ if (isset($_POST["requestType"]) && $_POST["requestType"] == "logout") {
         </div>
     </div>
 </nav>
-<!-- Modal -->
-<div class="modal" id="myModal" role="dialog">
-    <div class="modal-dialog">
-        <!-- Modal content-->
-        <div class="modal2-content">
-            <br>
+
+
+<!--Modal: Subscription From-->
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog cascading-modal" role="document">
+
+        <!--Content-->
+
+        <div class="modal-content">
+
             <form action="<?php echo $homedir; ?>pages/search/search.php" class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label" for="search">Search</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="search" placeholder="Enter Text Here" name="q">
-                    </div>
+
+            <!--Header-->
+
+            <div class="modal-header light-blue darken-3 white-text">
+
+                <h4 class="title"><i class="fa fa-search"></i>Search</h4>
+
+                <button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
+
+                    <span aria-hidden="true">&times;</span>
+
+                </button>
+
+            </div>
+
+            <!--Body-->
+
+            <div class="modal-body mb-0">
+
+
+
+                <div class="md-form form-sm">
+
+
+
+                    <input type="text" id="form27" class="form-control">
+
+
+
                 </div>
-                <div class="modal-footer">
-                    <input type="submit" value="Enter">
+
+                
+                <div class="text-center mt-1-half">
+
+                    <button class="btn btn-info mb-1">Submit <i class="fa fa-check ml-1"></i></button>
+
                 </div>
+
+
+
+            </div>
+
         </div>
+
+        <!--/.Content-->
+
     </div>
+
 </div>
+
+<!--Modal: Subscription From-->
