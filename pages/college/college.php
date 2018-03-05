634b5961e304af832a54b6855dad97124bd0ac47
NabaShauk <nshaukat@oakland.edu>
2018-03-05 16:55:11 -0500

    College Page edits
    Created the college profile page

--- pages/college/college.php
+++ pages/college/college.php
@@ -5,13 +5,10 @@
  * Date: 2/21/2018
  * Time: 9:00 PM
  */
-
 include "../../autoload.php";
-
 $controller = $_SESSION["controller"] = new Controller("MyCollege");
 $controller->initModuleDir();
 $controller->processREQUEST();
-
 //TODO: move this code into the controller class's processGET function
 if(isset($_GET["c"]) and is_numeric($_GET["c"])) {
     $college = new College($_GET["c"]);
@@ -23,51 +20,90 @@ if(isset($_GET["c"]) and is_numeric($_GET["c"])) {
 ?>
 <!DOCTYPE html>
 <html>
-    <title>MyCollege</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/college.min.css">
-    <body>
-        <?php include $controller->getHomeDir() . Controller::MODULE_DIR . "/pageassembly/header/header.php"; ?>
-        <!-- Overlay effect when opening sidebar on small screens -->
-        <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+<title>MyCollege</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<link rel="stylesheet" href="css/college.min.css">
+<body>
+<img src="/mycollege/files/7.jpg" id="bg" alt="">
 
-        <!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
-        <div class="w3-main" style="margin-left:250px">
+<?php include $controller->getHomeDir() . Controller::MODULE_DIR . "/pageassembly/header/header.php"; ?>
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
-            <div class="w3-row w3-padding-64">
-                <div class="w3-twothird w3-container">
-                    <h1 class="w3-text-teal"><?php echo $college->getName(); ?></h1>
-                    <p>ADD COLLEGE INFO HERE
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style="margin-left:200px">
 
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
-                        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat.</p>
-                </div>
-                <div class="w3-third w3-container">
-                    <p class="w3-border w3-padding-large w3-padding-64 w3-center">Picture or logo of school</p>
-                </div>
-            </div>
 
-            <!-- END MAIN -->
+    <div class="borderStyle">
+
+
+    <div class="w3-row w3-padding-64">
+
+        <div class="w3-twothird w3-container">
+            <h1 class="w3-text-teal"><?php echo $college->getName(); ?></h1>
+
         </div>
-        <!-- Sidebar -->
-        <nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" id="mySidebar">
-            <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
-                <i class="fa fa-remove"></i>
-            </a>
-            <h4 class="w3-bar-item"><b>Links to school website, etc</b></h4>
-            <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
-            <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
-            <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
-            <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
-        </nav>
-    </body>
+        <table>
+
+            <tbody>
+            <tr>
+            <td><i class="fas fa-globe fa-lg"></i>Location of College</td>
+            <td>ENType</td>
+            <td>ENSetting</td>
+            </tr>
+            <tr>
+                <td><i class="fas fa-male fa-lg"></i>Male %<i class="fas fa-female fa-lg"></i>
+                    Female %</td>
+                <td>Number of Students</td>
+                <td><i class="far fa-money-bill-alt fa-lg"></i>Tuition Cost</td>
+
+            </tr>
+            <tr>
+                <td>Acceptance Rate</td>
+                <td><i class="fas fa-first-aid fa-lg"></i>AVG Financial Aid</td>
+                <td># Of Professors</td>
+
+            </tr>
+
+
+            </tbody>
+        </table>
+
+
+
+        <div class="w3-third w3-container">
+            <p class="w3-border w3-padding-large w3-padding-64 w3-center">Picture or logo of school</p>
+            <p>List of Sports</p>
+        </div>
+    </div>
+
+    </div>
+
+
+
+    <!-- END MAIN -->
+</div>
+
+
+
+
+
+<!-- Sidebar -->
+<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" id="mySidebar">
+    <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
+        <i class="fa fa-remove"></i>
+    </a>
+    <h4 class="w3-bar-item"><b>Links to school website, etc</b></h4>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+    <a class="w3-bar-item w3-button w3-hover-black" href="#">Link</a>
+</nav>
+</body>
 </html>
