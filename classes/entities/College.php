4b329b82dbabf1958e8b0e526159dcaf9c134b61
Jacob <janick@oakland.edu>
2018-03-28 14:02:34 -0400

    Update College class with DB changes
    Changed where financial aid average was pulled from the DB. ~ 5 mins of work

--- classes/entities/College.php
+++ classes/entities/College.php
@@ -231,7 +231,7 @@ class College extends DataBasedEntity
                 $this->setPhone($college["nphone"]),//must occur after setProvince
                 $this->setTuitionIn($college["ninstate"]),
                 $this->setTuitionOut($college["noutstate"]),
-                $this->setFinAid($college["nfinancialave"]),
+                $this->setFinAid($finaid["naverage"]),
                 $this->setAcceptRate($college["nacceptrate"]),
                 $this->setProfCount($college["nprof"]),
                 $this->setStudentCount($college["nsize"]),
@@ -925,7 +925,7 @@ class College extends DataBasedEntity
         $dbc = new DatabaseConnection();
         if ($this->isInDatabase()) {
             $params = [
-                "ssssisiiiidiidiisi",
+                "ssssisiiidiidiisi",
                 $this->getName(),
                 $this->getType(),
                 $this->getStreetAddress(),
@@ -935,7 +935,6 @@ class College extends DataBasedEntity
                 $this->getPhone(),
                 $this->getTuitionIn(),
                 $this->getTuitionOut(),
-                $this->getFinAid(),
                 $this->getAcceptRate(),
                 $this->getProfCount(),
                 $this->getStudentCount(),
@@ -947,7 +946,7 @@ class College extends DataBasedEntity
             ];
             $result = $dbc->query("update", "UPDATE `tblcollege` SET 
                                   `nmcollege`=?,`entype`=?,`txstreetaddress`=?,`txcity`=?,`fkprovinceid`=?,`nzip`=?,
-                                  `nphone`=?,`ninstate`=?,`noutstate`=?,`nfinancialave`=?,`nacceptrate`=?,
+                                  `nphone`=?,`ninstate`=?,`noutstate`=?,`nacceptrate`=?,
                                   `nprof`=?,`nsize`=?,`nwomenratio`=?,`nact`=?,`nsat`=?,`ensetting`=?
                                   WHERE pkcollegeid = ?", $params);
 
@@ -966,7 +965,7 @@ class College extends DataBasedEntity
             $this->synced = $result;
         } else {
             $params = [
-                "ssssisiiiidiidiis",
+                "ssssisiiidiidiis",
                 $this->getName(),
                 $this->getType(),
                 $this->getStreetAddress(),
@@ -976,7 +975,6 @@ class College extends DataBasedEntity
                 $this->getPhone(),
                 $this->getTuitionIn(),
                 $this->getTuitionOut(),
-                $this->getFinAid(),
                 $this->getAcceptRate(),
                 $this->getProfCount(),
                 $this->getStudentCount(),
@@ -987,9 +985,9 @@ class College extends DataBasedEntity
             ];
             $result = $dbc->query("insert", "INSERT INTO `tblcollege`(`pkcollegeid`, `nmcollege`, `entype`, 
                                           `txstreetaddress`, `txcity`, `fkprovinceid`, `nzip`, `nphone`, 
-                                          `ninstate`, `noutstate`, `nfinancialave`, `nacceptrate`, `nprof`, `nsize`, 
+                                          `ninstate`, `noutstate`, `nacceptrate`, `nprof`, `nsize`, 
                                           `nwomenratio`, `nact`, `nsat`, `ensetting`)
-                                          VALUES  (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", $params);
+                                          VALUES  (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", $params);
 
             $result2 = $dbc->query("select", "SELECT LAST_INSERT_ID() AS lii");
             $this->setPkID($result2["lii"]);
