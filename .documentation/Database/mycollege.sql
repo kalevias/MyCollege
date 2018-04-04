f0bdd9c4fac4176d579945804f8ffb3289790011
Stephanie <sagorham@oakland.edu>
2018-04-04 18:06:14 -0400

    Deleted duplicates from GreekCollege
    Deleting duplicates

--- .documentation/Database/mycollege.sql
+++ .documentation/Database/mycollege.sql
@@ -1,13 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 03, 2018 at 05:13 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+﻿
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -1615,7 +1608,6 @@ INSERT INTO `tblgreekcollege` (`fkgreekid`, `fkcollegeid`) VALUES
 (82, 9),
 (88, 9),
 (89, 9),
-(92, 9),
 (92, 9),
 (94, 9),
 (97, 9),
